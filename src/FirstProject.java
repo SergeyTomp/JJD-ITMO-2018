3179a550f305297122b0e695437e6a4b44280649
Sergey Tomp <tsv_mail@mail.ru>
2018-03-20 02:14:33 +0400

    First source file

---
+++ src/FirstProject.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Сергей on 19.03.18.
+ */
+public class FirstProject {
+    public static void main (String [] args)
+    {
+        String line = "Hello World";
+        System.out.println(line);
+        System.out.println(5+3);
+    }
+}
