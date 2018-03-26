27b55db0364c37f75a3d972d17d570e47fc4b1f5
Sergey Tomp <tsv_mail@mail.ru>
2018-03-26 05:14:33 +0300

    HomeWork2

--- src/hw2/Task11.java
+++ src/hw2/Task11.java
@@ -1,6 +1,6 @@
 package hw2;
 //Требуется все числа, которые встречаются в строке, поместить в отдельный целочисленный
-// НЕ ДОДЕЛАЛ, КАКАЯ-ТО ТУПАЯ ОШИБКА, ПОКА НЕ ПОНИМАЮ
+// ужасно тупая реализация, но ничего другого не осилил, увы :-(, на свежую голову вернусь
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,17 +10,19 @@ public class Task11 {
         String str1;
         String symb;
         int arr_sz = 0;
-        char tmp_symb;
+//        char tmp_symb;
         System.out.println("Введите строку");
         Scanner keyb = new Scanner(System.in);
         str1 = keyb.nextLine();
         //вычисляем размер массива чисел
         for (int i=0; i<str1.length(); i++){
-            tmp_symb = str1.charAt(i);
-            if (tmp_symb >= '0' && tmp_symb <= '9') {
-                for (i+=1; i<str1.length(); i++){
-                    tmp_symb = str1.charAt(i);
-                    if (tmp_symb >= '0' && tmp_symb <= '9'){;}
+            if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9') {
+                for (; i<str1.length(); i++){
+                    if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9') {
+                        if (i == str1.length() - 1) {
+                            arr_sz += 1;
+                        }
+                    }
                     else {
                         i-=1;
                         arr_sz += 1;
@@ -29,19 +31,18 @@ public class Task11 {
                 }
             }
         }
-        System.out.println(arr_sz);
         //наполняем массив
         int [] nums_arr = new int[arr_sz];
         int nums_arr_indx = 0;
         for (int i=0; i<str1.length(); i++){
-            tmp_symb = str1.charAt(i);
-            if (tmp_symb >= '0' && tmp_symb <= '9'){
+            if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9'){
                 StringBuffer str_buf = new StringBuffer();
-                str_buf.append(tmp_symb);
-                for (i+=1; i<str1.length(); i++){
-                    tmp_symb = str1.charAt(i);
-                    if (tmp_symb >= '0' && tmp_symb <= '9'){
-                        str_buf.append(tmp_symb);
+                for (; i<str1.length(); i++){
+                    if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9'){
+                        str_buf.append(str1.charAt(i));
+                        if (i == str1.length() - 1) {
+                            nums_arr [nums_arr_indx] = Integer.valueOf(str_buf.toString());
+                        }
                     }
                     else {
                         i-=1;
