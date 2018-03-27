b5f9db724e71299b2f229c840373de9da85686ee
Sergey Tomp <tsv_mail@mail.ru>
2018-03-27 22:17:24 +0300

    HomeWork2

--- src/hw2/Task10.java
+++ src/hw2/Task10.java
@@ -6,9 +6,6 @@ import java.lang.StringBuffer;
 
 public class Task10 {
     public static void main(String[] args) {
-
-        //решаем двумя способами
-
         String str;
         String symb;
         System.out.println("Введите строку");
@@ -16,14 +13,14 @@ public class Task10 {
         str = keyb.nextLine();
         str = str.replace(" ", "");
 
-        // Способ №1: чуть-чуть Java
-
+        //дальше решаем двумя способами
+        // Способ №1: чуть-чуть из Java, остальное как обычно
         StringBuffer str_buf = new StringBuffer(str);
-        for (int i=0; i<str_buf.length(); i++){
-            symb = str_buf.substring(i,i + 1);
-            for (int j=i+1; j<str_buf.length(); j++){
+        for (int i = 0; i < str_buf.length(); i++) {
+            symb = str_buf.substring(i, i + 1);
+            for (int j = i + 1; j < str_buf.length(); j++) {
                 int indx = str_buf.indexOf(symb, j);
-                if (indx >= 0){
+                if (indx >= 0) {
                     str_buf.deleteCharAt(indx);
                     j = indx - 1;
                 }
@@ -31,14 +28,15 @@ public class Task10 {
         }
         System.out.println(str_buf);
 
-        // Способ №2: целиком Java
+        // Способ №2: целиком на Java
         // свойство LinkedHashSet - можно добавить только неповторяющийся элемент, при этом сохранаятся порядок добавления
-
-        LinkedHashSet <Character> repeatIgnore = new LinkedHashSet<>();
+        LinkedHashSet<Character> repeatIgnore = new LinkedHashSet<>();
         str_buf = new StringBuffer();
-        char [] strToArr = str.toCharArray();
-        for (char tmp : strToArr){
-            if (repeatIgnore.add(tmp)){
+        char[] strToArr = str.toCharArray();
+        for (char tmp : strToArr) {
+            //из объекта LinkedHashSet строка выводится криво, типа как Arrays.toString, поэтому клеим строку отдельно в stringBuffer
+            // если LinkedHashSet принимает символ, добавляем его же в stringBuffer, если нет, это повтор, в корзину его
+            if (repeatIgnore.add(tmp)) {
                 str_buf.append(tmp);
             }
         }
--- src/hw2/Task11.java
+++ src/hw2/Task11.java
@@ -6,16 +6,12 @@ import java.util.Scanner;
 public class Task11 {
     public static void main(String[] args) {
         String str;
-        String symb;
-        int arr_sz = 0;
         System.out.println("Введите строку");
         Scanner keyb = new Scanner(System.in);
         str = keyb.nextLine();
-        //делаем всё за один проход:
-        // заменяем все нецифровые символы на пробелы,
-        // обрезаем пустоты по краям
-        // разрезаем результат по пробелам, получаем массив строк из чисел
-        String [] cleanRubbish = str.replaceAll("\\D+", " ").trim().split(" ");
+        str = str.replaceAll("\\D+", " "); // заменяем все нецифровые символы на пробелы,
+        str = str.trim();                                  // обрезаем пробелы по краям
+        String [] cleanRubbish = str.split(" ");      // разрезаем результат по пробелам, получаем массив строк из чисел
         int [] result = new int[cleanRubbish.length];
         for (int i=0; i<result.length; i++) {
             result[i] = Integer.parseInt(cleanRubbish[i]);
