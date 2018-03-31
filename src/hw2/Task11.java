9886d35cfbcbed4e3ea98cd987bc1aeb3c71f467
Sergey Tomp <tsv_mail@mail.ru>
2018-03-31 20:25:38 +0300

    домашнее задание от 23/03/18

--- src/hw2/Task11.java
+++ src/hw2/Task11.java
@@ -9,9 +9,9 @@ public class Task11 {
         System.out.println("Введите строку");
         Scanner keyb = new Scanner(System.in);
         str = keyb.nextLine();
-        str = str.replaceAll("\\D+", " "); // заменяем все нецифровые символы на пробелы,
-        str = str.trim();                                  // обрезаем пробелы по краям
-        String [] cleanRubbish = str.split(" ");      // разрезаем результат по пробелам, получаем массив строк из чисел
+        str = str.replaceAll("\\D+", " ");// заменяем все нецифровые символы на пробелы,
+        str = str.trim();                                 // обрезаем пробелы по краям
+        String [] cleanRubbish = str.split(" ");    // разрезаем результат по пробелам, получаем массив строк из чисел
         int [] result = new int[cleanRubbish.length];
         for (int i=0; i<result.length; i++) {
             result[i] = Integer.parseInt(cleanRubbish[i]);
